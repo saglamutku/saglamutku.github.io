f2fcbd4e2923d3680bc8b1adb42ed705d088c10e
utkusaglam <saglmutku@gmail.com>
2025-04-02 20:32:09 +0200

    added config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+const LIVE_URL = "https://saglamutku.github.io"
 // https://astro.build/config
 export default defineConfig({
+  site: LIVE_URL,
   vite: {
     plugins: [tailwindcss()],
   },
